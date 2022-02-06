e785ece79abe69d527770e9d56e7e560a0b2ee4b
anjue39 <98279987+anjue39@users.noreply.github.com>
2022-02-06 22:51:24 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,7 +14,3 @@
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 sed -i 's/OpenWrt/LEDE/g' package/base-files/files/bin/config_generate
 
-# Add & replace luci-theme-argon
-cd package/lean  
-rm -rf luci-theme-argon  
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  
