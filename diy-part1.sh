df6e45a1e687ba4b5d19593cf1aed160eb18c83d
anjue39 <98279987+anjue39@users.noreply.github.com>
2022-03-06 21:43:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -30,6 +30,11 @@ rm -rf package/lean/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/lean/luci-theme-argon
 echo '=========Add argon-mod OK!========='
 
+echo '添加jerrykuku的argon-mod主题自定义配置'
+rm -rf package/lean/luci-app-argon-config 
+git clone https://github.com/jerrykuku/luci-app-argon-config package/lean/luci-app-argon-config
+echo '=========Add argon-mod config OK!========='
+
 echo '移除bcm53xx中的其他机型'
 sed -i '421,453d' target/linux/bcm53xx/image/Makefile
 sed -i '140,412d' target/linux/bcm53xx/image/Makefile
@@ -37,7 +42,17 @@ sed -i 's/$(USB3_PACKAGES) k3screenctrl/luci-app-k3screenctrl/g' target/linux/bc
 # sed -n '140,146p' target/linux/bcm53xx/image/Makefile
 echo '=========Remove other devices of bcm53xx OK!========='
 
+echo '修改主机名'
+sed -i "s/hostname='OpenWrt'/hostname='Phicomm-K3'/g" package/base-files/files/bin/config_generate
+cat package/base-files/files/bin/config_generate |grep hostname=
+echo '=========Alert hostname OK!========='
 
+echo '修改路由器默认IP'
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+echo '=========Alert default IP OK!========='
+
+# K3专用，编译K3的时候只会出K3固件（去掉sed前面的#生效）
+sed -i 's|^TARGET_|# TARGET_|g; s|# TARGET_DEVICES += phicomm-k3|TARGET_DEVICES += phicomm-k3|' target/linux/bcm53xx/image/Makefile
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
