498110426dfe8a6e21edccb03367626b097ecbdd
anjue39 <98279987+anjue39@users.noreply.github.com>
2022-03-08 21:42:35 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -30,16 +30,3 @@
 # rm -rf luci-theme-argon  
 # git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
 
-echo '修改主机名'
-sed -i "s/hostname='OpenWrt'/hostname='LEDE'/g" package/base-files/files/bin/config_generate
-cat package/base-files/files/bin/config_generate |grep hostname=
-echo '=========Alert hostname OK!========='
-
-echo '修改路由器默认IP'
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-echo '=========Alert default IP OK!========='
-
-# 修改插件名
-sed -i 's/"KMS 服务器"/"KMS激活"/g' `grep "KMS 服务器" -rl ./`
-sed -i 's/"USB 打印服务器"/"打印服务"/g' `grep "USB 打印服务器" -rl ./`
-sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' `grep "Turbo ACC 网络加速" -rl ./`
