e76f9ae0f3ee63b88796708bf08f144bdc9c7613
anjue39 <98279987+anjue39@users.noreply.github.com>
2022-03-09 16:01:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -60,29 +60,6 @@ echo '移除主页跑分信息显示'
 sed -i 's/ <%=luci.sys.exec("cat \/etc\/bench.log") or ""%>//g' package/lean/autocore/files/arm/index.htm
 echo '=========Remove benchmark display in index OK!========='
 
-echo '修改主机名'
-sed -i "s/hostname='OpenWrt'/hostname='PHICOMM'/g" package/base-files/files/bin/config_generate
-cat package/base-files/files/bin/config_generate |grep hostname=
-echo '=========Alert hostname OK!========='
-
-echo '修改路由器默认IP'
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-echo '=========Alert default IP OK!========='
-
-# 修改插件名字
-# sed -i 's/"aMule设置"/"电驴下载"/g' `grep "aMule设置" -rl ./`
-# sed -i 's/"网络存储"/"NAS"/g' `grep "网络存储" -rl ./`
-sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' `grep "Turbo ACC 网络加速" -rl ./`
-# sed -i 's/"实时流量监测"/"流量"/g' `grep "实时流量监测" -rl ./`
-# sed -i 's/"KMS 服务器"/"KMS激活"/g' `grep "KMS 服务器" -rl ./`
-# sed -i 's/"TTYD 终端"/"命令窗"/g' `grep "TTYD 终端" -rl ./`
-sed -i 's/"USB 打印服务器"/"打印服务"/g' `grep "USB 打印服务器" -rl ./`
-# sed -i 's/"Web 管理"/"Web"/g' `grep "Web 管理" -rl ./`
-# sed -i 's/"管理权"/"改密码"/g' `grep "管理权" -rl ./`
-# sed -i 's/"带宽监控"/"监控"/g' `grep "带宽监控" -rl ./`
-sed -i 's/"Argon 主题设置"/"Argon设置"/g' `grep "Argon 主题设置" -rl ./`
-
-
 
 # echo 'K3专用，编译K3的时候只会出K3固件（去掉sed前面的#生效）'
 # sed -i 's|^TARGET_|# TARGET_|g; s|# TARGET_DEVICES += phicomm-k3|TARGET_DEVICES += phicomm-k3|' target/linux/bcm53xx/image/Makefile
